7c75cada33cf9ad0659ac20e0c8bf5b6ba65329f
Darshitshyani <darshitshyani1@gmail.com>
2025-09-22 17:59:36 +0530

    fix build issue

--- src/pages/services/[index].tsx
+++ src/pages/services/[index].tsx
@@ -1,22 +1,11 @@
-import Digitalproductdesign from '@/components/services/Digitalproductdesign'
-import EngineeringDevOps from '@/components/services/EngineeringDevOps'
-import MobileDevelopment from '@/components/services/mobileDevelopment'
-import SoftwareAct from '@/components/services/SoftwareAct'
-import { useRouter } from 'next/router'
-import React from 'react'
+import { memo } from 'react';
 
-const index = () => {
-  const router = useRouter()
-  console.log(router.query.index)
+const Index = () => {
   return (
-    <div>
-   
-    {router.query.index === "digital-product-design"&&  <Digitalproductdesign />}
-     {router.query.index === "software-architecture" && <SoftwareAct />}
-     {router.query.index === "engineering-devops" && <EngineeringDevOps />}
-     {router.query.index === "mobile-app-development" && <MobileDevelopment />}
+    <div className="Index">
+      <h2>Index</h2>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default memo(Index);
